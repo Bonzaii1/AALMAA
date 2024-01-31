47cae430cd4dc2591905aa475ce5f6d80cae3361
Matt <mattherr23@gmail.com>
2024-01-31 12:16:31 -0600

    added button and search input, no list yet

--- frontend/src/pages/Brigada.jsx
+++ frontend/src/pages/Brigada.jsx
@@ -2,9 +2,15 @@
 
 const Brigada = () => {
     return (
-        <section>
-            Brigada
-        </section>
+
+        <div className="w-full h-full flex flex-col">
+            <div className="flex justify-between">
+                <button className="bg-gradient-to-r from-[#00c6ff] to-[#0072ff] p-2 rounded-md text-white">Agregar Brigada</button>
+                <input className="p-2 rounded-md" type="text" id="search" name="search" placeholder="Search"></input>
+            </div>
+
+        </div>
+
     )
 }
 
